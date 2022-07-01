b43d0ed61f500b46765df01c28b910eb5171d998
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-07-01 21:41:29 +0900

    修正　４→６

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     birthday              {Faker::Date.birthday}
 
