e3574bb9d31eb34cc0d82923065beabaf26b0652
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-07-08 18:25:42 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :item_set, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :item_set, only: [:show, :edit, :update, :destroy]
 
   def index
 
@@ -39,7 +39,12 @@ end
      end
    end
 
-
+   def destroy
+    if current_user == @item.user
+      @item.destroy
+    end
+    redirect_to root_path
+   end
 
 
 
