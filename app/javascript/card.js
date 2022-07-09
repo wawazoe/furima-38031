25ade007d676edb77ba5ece5396a0929d2ea16bc
wawazoe <ngycxzbsnbf508j08gpb@docomo.ne.jp>
2022-07-09 15:32:10 +0900

    PAY.JPのカードの情報をトークン化まで　
    次はブラウザで確認から

---
+++ app/javascript/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_asdfghjkl123"); // PAY.JPテスト公開鍵
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
